5708417ef992be60658f4420916605b239038083
Paul Diepold <paul.diepold@outlook.de>
2025-12-23 00:27:33 +0100

    udpate nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -22,10 +22,14 @@ export default defineNuxtConfig({
 
   runtimeConfig: {
     allowedUsers: process.env.NUXT_ALLOWED_USERS || '',
-    oauthGithubClientSecret: process.env.NUXT_OAUTH_GITHUB_CLIENT_SECRET || '',
-    sessionPassword: process.env.NUXT_SESSION_PASSWORD || '',
-    public: {
-      oauthGithubClientId: process.env.NUXT_OAUTH_GITHUB_CLIENT_ID || '',
+    oauth: {
+      github: {
+        clientId: process.env.NUXT_OAUTH_GITHUB_CLIENT_ID || '',
+        clientSecret: process.env.NUXT_OAUTH_GITHUB_CLIENT_SECRET || '',
+      },
+    },
+    session: {
+      password: process.env.NUXT_SESSION_PASSWORD || '',
     },
   },
 
