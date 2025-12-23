ac3edfdd251e57fc5019341dd76bf130cf7400c4
pauldiepold <37743097+pauldiepold@users.noreply.github.com>
2025-12-23 09:02:50 +0100

    Add preview deployment (#6)
    * change d1 database
    
    * update wrangler config
    
    * udpate nuxt config
    
    * udpate nuxt config
    
    * udpate nuxt config
    
    * udpate nuxt config
    
    * udpate nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -22,6 +22,15 @@ export default defineNuxtConfig({
 
   runtimeConfig: {
     allowedUsers: process.env.NUXT_ALLOWED_USERS || '',
+    oauth: {
+      github: {
+        clientId: process.env.NUXT_OAUTH_GITHUB_CLIENT_ID || '',
+        clientSecret: process.env.NUXT_OAUTH_GITHUB_CLIENT_SECRET || '',
+      },
+    },
+    session: {
+      password: process.env.NUXT_SESSION_PASSWORD || '',
+    },
   },
 
   compatibilityDate: '2025-12-11',
