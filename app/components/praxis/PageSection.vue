07ebfc200b714774c9a98a929230e4165f2c93a8
Paul Diepold <paul.diepold@outlook.de>
2026-02-05 21:57:18 +0100

    a lot of work

---
+++ app/components/praxis/PageSection.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+defineProps<{
+  id: string
+  heading: string
+  bgClass: string
+}>()
+</script>
+
+<template>
+  <section
+    aria-labelledby="kontakt"
+    class="py-10"
+    :class="bgClass"
+  >
+    <UContainer class="py-10 space-y-10">
+      <PraxisSectionHeading
+        :id="id"
+        :heading="heading"
+      />
+    </UContainer>
+  </section>
+</template>
